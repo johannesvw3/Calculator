bbd7a7c15e2c0a7f13ab76fa4704ea881cd2bc5f
johannesvw3 <64883700+johannesvw3@users.noreply.github.com>
2021-02-18 13:25:01 -0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -175,3 +175,15 @@ decimalEl.addEventListener('click', () => {
   }
 });
 
+//change Color
+const buttons = document.querySelectorAll("button");
+ 
+ for (button in buttons) {
+    buttons[button].onclick = function() {
+        buttons.forEach(function(btn){
+          btn.classList.remove('highlight');
+        })
+        this.classList.add('highlight');
+    }
+}
+
